c44c186d5aa5c835a71ab39efe4da13be6475ac8
aliceMungamuri <158488797+aliceMungamuri@users.noreply.github.com>
2024-10-05 10:09:47 -0500

    Update mapScript.js

--- mapScript.js
+++ mapScript.js
@@ -4,9 +4,9 @@ let map;
 // Function to initialize the map
 function initMap() {
     // Set default location and zoom level
-    const cityLocation = { lat:38.97543,lng:95.2558}; // Lawrence Kansas
+    const cityLocation = { lat:38.9543,lng:95.2558}; // Lawrence Kansas
     map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 12, // the higher the number the closer the map will be as its default
+        zoom: 14, // the higher the number the closer the map will be as its default
         center: cityLocation,
     });
 
